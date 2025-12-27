fa087b0df55e1f4aa27499b87118d8a28b9de604
dan <you@example.com>
2025-12-27 13:51:06 -0300

    cookies.txt support

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,10 @@
 import subprocess
+import os
 
 async def get_vid(video):
-    return subprocess.check_output(["yt-dlp", "--no-warnings", "-f", "best/bestvideo+bestaudio", "--get-url", video])
+    if os.path.isfile("config/cookies.txt"):
+        print("cookie")
+        return subprocess.check_output(["yt-dlp", "--cookies", "cookies.txt", "--no-warnings", "-f", "best/bestvideo+bestaudio", "--get-url", video])
+    else:
+        return subprocess.check_output(["yt-dlp", "--no-warnings", "-f", "best/bestvideo+bestaudio", "--get-url", video])
+
